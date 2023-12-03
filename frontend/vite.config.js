5fbdc157d8a5bcb39767449fadc8f0ae79d27a1b
Vaishnav Prasad R K <88769806+rkvaishnavp@users.noreply.github.com>
2023-12-03 10:09:08 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             "/api": {
-                target: process.env.RENDER_URL || process.env.PORT,
+                target: "http://127.0.0.1:5000",
                 changeOrigin: true,
                 secure: false,
             },
